0f506c3300fccd64f512b2d8c7e03e6489088d9c
Daniel Fajtai <daniel.fajtai@gmail.com>
2024-07-23 10:18:34 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -7,8 +7,9 @@ var font_paths = [
 ];
 
 var start_time = Object();
-var session_max_duration = moment.duration(30, "minutes");
+const session_max_duration = moment.duration(60, "minutes");
 var session_countdown = Object();
+var is_loading_timeout = null;
 
 var default_mask_calendar_name = "MR előjegyzés maszk";
 var default_calendar_name = "MR előjegyzés";
