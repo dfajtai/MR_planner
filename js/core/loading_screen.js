9d1b9f6236d92ffff262f02d600d3e5288005637
Daniel Fajtai <daniel.fajtai@gmail.com>
2024-07-23 14:06:51 +0200

    Update loading_screen.js

--- js/core/loading_screen.js
+++ js/core/loading_screen.js
@@ -2,6 +2,7 @@ function is_loading(val) {
 	if (val) {
 		$(".loading-overlay").removeClass("d-none");
 		is_loading_timeout = setTimeout(function () {
+			is_loading(false);
 			bootbox.alert({
 				message: "Az EWS szerver nem válaszol.",
 				buttons: {
@@ -10,9 +11,7 @@ function is_loading(val) {
 						className: "btn-outline-dark",
 					},
 				},
-				callback: function (reusult) {
-					is_loading(false);
-				},
+				callback: function (reusult) {},
 			});
 		}, 60000);
 	} else {
